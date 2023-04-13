09d5edc73456e40e646eab4b865966f68d584d4b
Kagema Njoroge <reecejames934@gmail.com>
2023-04-13 16:00:02 +0300

    Remove typo from Views file

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from .models import Course
+from .models import Courses
 # Create your views here.
 
 def get_courses(request):
