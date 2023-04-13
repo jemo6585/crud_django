99f9a04369cd589533990fa240753d2955a68dd8
Kagema Njoroge <reecejames934@gmail.com>
2023-04-13 16:00:35 +0300

    Remove typo from Views file

--- courses/views.py
+++ courses/views.py
@@ -4,9 +4,9 @@ from .models import Courses
 # Create your views here.
 
 def get_courses(request):
-    courses = Course.objects.all()
+    courses = Courses.objects.all()
     return JsonResponse(courses, safe=False)
 
 def get_course(request, course_id):
-    course = Course.objects.get(id=course_id)
+    course = Courses.objects.get(id=course_id)
     return JsonResponse(course, safe=False)
