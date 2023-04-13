57aa04a0dd1ab7b9d21bc177379b45b3b31133a4
Kagema Njoroge <reecejames934@gmail.com>
2023-04-13 15:52:55 +0300

    Create view to get all course objects

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,8 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-
+from .models import Course
 # Create your views here.
 
-
+def get_courses(request):
+    courses = Course.objects.all()
+    return JsonResponse(courses, safe=False)
