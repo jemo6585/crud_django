abba774184ac4102fee92e8ceaabaeecc067e292
Kagema Njoroge <reecejames934@gmail.com>
2023-04-13 15:54:38 +0300

    Create view to get a single Course object provided a course id

--- courses/views.py
+++ courses/views.py
@@ -6,3 +6,7 @@ from .models import Course
 def get_courses(request):
     courses = Course.objects.all()
     return JsonResponse(courses, safe=False)
+
+def get_course(request, course_id):
+    course = Course.objects.get(id=course_id)
+    return JsonResponse(course, safe=False)
