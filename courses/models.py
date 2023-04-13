0381363d64264ead39aa0d879318af6ff2863237
Kagema Njoroge <reecejames934@gmail.com>
2023-04-13 15:33:02 +0300

    Removed typo from Courses model

--- courses/models.py
+++ courses/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Courses(models.Model):
-    couse_id = models.IntegerField(primary_key=True,auto_created=True)
+    course_id = models.IntegerField(primary_key=True, auto_created=True)
     course_name = models.CharField(max_length=200)
     course_description = models.TextField()
 
