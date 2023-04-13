7bd26eb55cc42dbbb8b34de09aa2594453334780
Kagema Njoroge <reecejames934@gmail.com>
2023-04-13 15:30:43 +0300

    Created Courses model

--- courses/models.py
+++ courses/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class Courses(models.Model):
+    couse_id = models.IntegerField(primary_key=True,auto_created=True)
+    course_name = models.CharField(max_length=200)
+    course_description = models.TextField()
+
+    date_created = models.DateTimeField(auto_now_add=True)
